5fe42d167471bea4f64106647972e6842859cc77
MoriaCohen8 <103572180+MoriaCohen8@users.noreply.github.com>
2025-04-29 17:28:21 +0300

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -15,10 +15,20 @@ app.use((req, res, next) => {
 });
 const corsConfig = {
   origin: "https://recipe-web-front.onrender.com",
-  credentials: true
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
 app.use(cors(corsConfig));
 app.options("*", cors(corsConfig));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://recipe-web-front.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 
 app.use(logger("dev")); //logger
 app.use(express.json()); // parse application/json
