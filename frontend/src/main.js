63e13dac83dcace293c36cc8eb64e62ee5c87cdb
MoriaCohen8 <103572180+MoriaCohen8@users.noreply.github.com>
2025-04-30 11:36:55 +0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,6 @@ axios.defaults.withCredentials = true;
 import VueScrollFixedNavbar from "vue-scroll-fixed-navbar";
 
 Vue.use(VueScrollFixedNavbar);
-
 import routes from "./routes";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
