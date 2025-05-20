a6ee7f422322a8d58e05acc6daed5f16093f3136
MoriaCohen8 <103572180+MoriaCohen8@users.noreply.github.com>
2025-05-20 16:36:32 +0300

    Update MainPage.vue

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,53 +1,21 @@
 <template>
   <div class="container">
-    <br />
-    <div id="welcome" class="text-center">
-      <img src="@/assets/welcome2.png" width="50%" />
-    </div>
-
+    <br><div id="welcome" justify="center"><center><img src="@/assets/welcome2.png" width ="50%" height = "0%" /></center></div>
     <b-row>
-      <b-col></b-col>
-      <b-col></b-col>
-
+      <b-col></b-col> <b-col></b-col>
       <b-col cols="5" class="RandomRecipesCol">
-        <div v-if="isLoading" class="loader-container">
-          <b-spinner variant="warning" label="Loading..." class="my-3" />
-          <p>This may take a few seconds...</p>
-        </div>
-
-        <RecipePreviewList
-          v-else
-          route_name="/recipes/random"
-          title="Explore this recipes"
-          class="RandomRecipes center"
-          @loaded="isLoading = false"
-        />
-
-        <b-button tag="button" pill @click="new_random_recipes" class="buttonwelc">
-          Click for new 3 recipes!
-        </b-button>
+        <RecipePreviewList route_name="/recipes/random" title="Explore this recipes" class="RandomRecipes center" />
+        <b-button tag="button" pill @click="new_random_recipes" class="buttonwelc"> Click for new 3 recipes!</b-button>
       </b-col>
-
       <b-col></b-col>
-
       <b-col cols="4">
-        <RecipePreviewList
-          v-if="$root.store.username"
-          route_name="/users/getThreeLast"
-          title="Last watched recipes"
-          class="LastViewedRecipes center"
-        />
-        <LogIn
-          v-else
-          width="200px"
-          title="Log In"
-          class="Log In center"
-        />
+        <RecipePreviewList v-if="$root.store.username" route_name="/users/getThreeLast" title="Last watched recipes" class="LastViewedRecipes center" ></RecipePreviewList>
+        <!-- <RecipePreviewList v-if="!$root.store.username" title="Log in and Register" class="RandomRecipes center" /> -->
+        <!-- <br><b-button v-if="!$root.store.username" to="/login" tag="button" class="buttonwelc" pill>Click to log in!</b-button>
+        <br><br><b-button v-if="!$root.store.username" to="/Register" tag="button" class="buttonwelc" pill>Click to register!</b-button> -->
+        <LogIn v-if="!$root.store.username" width="200px" title="Log In" class="Log In center" />
       </b-col>
-
-      <b-col></b-col>
-      <b-col></b-col>
-      <b-col></b-col>
+      <b-col></b-col><b-col></b-col><b-col></b-col>
     </b-row>
   </div>
 </template>
@@ -56,51 +24,46 @@
 import RecipePreviewList from "../components/RecipePreviewList";
 import LogIn from "../components/LogIn.vue";
 
+
 export default {
   components: {
     RecipePreviewList,
     LogIn
   },
-  data() {
-    return {
-      isLoading: true
-    };
-  },
-  methods: {
-    new_random_recipes() {
-      this.isLoading = true;
-      setTimeout(() => {
-        window.location.reload();
-      }, 200);
+   methods: {
+    new_random_recipes(){
+      // this.$children.updateRandomRecipes()
+      window.location.reload();
+
     }
-  }
+    }
 };
 </script>
 
 <style lang="scss" scoped>
+
 .container {
   border-radius: 27px;
   max-width: 1900px;
 }
-
 .center {
   margin-left: auto;
   margin-right: auto;
 }
 
-.title {
-  color: #cfa343;
+.title{
+  color:#cfa343;
   font-weight: bolder;
-}
 
-.big-title {
-  font-family: Georgia, 'Times New Roman', Times, serif;
+}
+.big-title{
+  font-family:Georgia, 'Times New Roman', Times, serif;
   font-size: 48px;
-  color: #cfa343;
+  color:#cfa343;
 }
 
-.buttonwelc {
-  width: 40%;
+.buttonwelc{
+  width:40%;
   font-family: Georgia, serif;
   font-size: 23px;
   padding: 14px 14px;
@@ -109,7 +72,7 @@ export default {
   font-weight: bold;
   color: #080807;
   margin-left: 235px;
-  border-radius: 50%;
+  border-radius: '50%';
 }
 
 .buttonwelc:hover {
@@ -117,17 +80,11 @@ export default {
   color: white;
 }
 
-.log {
-  font-family: Georgia, 'Times New Roman', Times, serif;
+.log{
+  font-family:Georgia, 'Times New Roman', Times, serif;
   font-size: 48px;
-  color: #cfa343;
+  color:#cfa343;
   font-weight: bolder;
 }
 
-.loader-container {
-  text-align: center;
-  margin-top: 30px;
-  font-size: 18px;
-  color: #cfa343;
-}
 </style>
